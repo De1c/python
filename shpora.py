a886fe486730a8ff12e9c4f30ab5241879c543c1
De1c <denpro8350@gmail.com>
2022-09-07 19:44:16 +0200

    Update shpora.py

--- shpora.py
+++ shpora.py
@@ -1,7 +1,25 @@
+import timeit
+
 def func_one(n):
     return [str(num) for num in range(n)]
 
 def func_two(n):
     return list(map(str, range(n)))
 
-print(func_two(100000))
+
+setup = '''
+def func_one(n):
+    return [str(num) for num in range(n)]
+'''
+stmt = 'func_one(100)'
+
+print(timeit.timeit(stmt, setup, number=1000))
+
+setup2 = '''
+def func_two(n):
+    return list(map(str,range(n)))
+'''
+
+stmt2 = 'func_two(100)'
+
+print(timeit.timeit(stmt2, setup2, number=1000))
